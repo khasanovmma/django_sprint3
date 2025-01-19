8b22d9b5b946f463ec9466ed0363b7e9f201e90f
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-01-19 20:18:08 +0500

    refact: remove unnecessary package

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -13,7 +13,10 @@ class PublishedModel(models.Model):
 
 
 class CreatedAtModel(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Добавлено")
+    created_at = models.DateTimeField(
+        auto_now_add=True,
+        verbose_name="Добавлено",
+    )
 
     class Meta:
         abstract = True
