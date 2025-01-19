a15e88cec08c48e2bd6442cea82d8d10640e60b6
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-01-19 20:36:21 +0500

    fixbug: fix bug after pytest

--- blogicum/blog/models.py
+++ blogicum/blog/models.py
@@ -24,8 +24,10 @@ class Category(PublishedModel, CreatedAtModel, TitleModel):
     slug = models.SlugField(
         unique=True,
         verbose_name="Идентификатор",
-        help_text="""Идентификатор страницы для URL; разрешены символы
-латиницы, цифры, дефис и подчёркивание.""",
+        help_text=(
+            "Идентификатор страницы для URL; разрешены символы "
+            "латиницы, цифры, дефис и подчёркивание."
+        ),
     )
 
     class Meta:
@@ -40,8 +42,10 @@ class Post(PublishedModel, CreatedAtModel, TitleModel):
     text = models.TextField(verbose_name="Текст")
     pub_date = models.DateTimeField(
         verbose_name="Дата и время публикации",
-        help_text="""Если установить дату и время в будущем — можно делать
-отложенные публикации.""",
+        help_text=(
+            "Если установить дату и время в будущем — "
+            "можно делать отложенные публикации."
+        ),
     )
     author = models.ForeignKey(
         User,
