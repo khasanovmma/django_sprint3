a9519ee01e016a6ef3c5ae6fdbb6722543e19a90
Abdulboriy Khasanov <khasanovmma010@gmail.com>
2025-01-19 20:06:49 +0500

    feat: implement post by category

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from django.utils import timezone
 
-from .models import Post
+from .models import Category, Post
 
 
 def index(request):
@@ -40,8 +40,24 @@ def post_detail(request, pk):
 
 def category(request, category_slug):
     template = "blog/category.html"
+    category_obj = get_object_or_404(
+        Category,
+        slug=category_slug,
+        is_published=True,
+    )
+    post_list = (
+        Post.objects.filter(
+            is_published=True,
+            pub_date__lte=timezone.now(),
+            category__is_published=True,
+            category=category_obj,
+        )
+        .select_related("category")
+        .order_by("-pub_date")
+    )
+
     return render(
         request=request,
         template_name=template,
-        context={"category_slug": category_slug},
+        context={"category": category_obj, "post_list": post_list[:5]},
     )
